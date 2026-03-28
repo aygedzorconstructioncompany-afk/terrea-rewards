981e8d4ff34588a81a352b9c06a03886c6d44302
David <aygedzorconstructioncompany@gmail.com>
2026-03-28 17:45:55 +0400

    add index route

--- app/routes/app/routes.ts
+++ app/routes/app/routes.ts
@@ -1,9 +1,6 @@
-import { type RouteConfig, route } from "@react-router/dev/routes";
+import { route } from "@react-router/dev/routes";
 
 export default [
-  route("/", "routes/index.tsx"),
-
-  route("/app", "routes/app._index.tsx"),
   route("/app/rewards", "routes/app/rewards.tsx"),
   route("/test", "routes/app/test.tsx"),
 ];
