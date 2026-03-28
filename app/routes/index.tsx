4aeee32dd31997ad4cb3026c768e955f0faddf40
David <aygedzorconstructioncompany@gmail.com>
2026-03-28 18:47:18 +0400

    remove index folder

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,3 @@
 export default function Index() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Terrea Rewards App</h1>
-      <p>App is working ✅</p>
-    </div>
-  );
+  return <div>Home работает ✅</div>;
 }
