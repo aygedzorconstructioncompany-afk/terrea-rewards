ec575bf1275c080344f3ea7b88195f30959a2219
aygedzorconstructioncompany-afk <aygedzorconstructioncompany@gmail.com>
2026-04-21 16:27:40 +0400

    Update api.points.transfer.ts

--- web/app/routes/api.points.transfer.ts
+++ web/app/routes/api.points.transfer.ts
@@ -17,7 +17,7 @@ export async function action({ request }: any) {
 
   try {
     const { customer_id, to_email, points, shop } = await request.json();
-    const shopId = shop || "terrea-dev-store.myshopify.com";
+   const shopId = shop || process.env.SHOPIFY_SHOP_DOMAIN || "terrea-home-rituals.myshopify.com";
 
     if (!customer_id || !to_email || !points) {
       return Response.json({ error: "Missing data" }, { status: 400, headers: corsHeaders(request) });
