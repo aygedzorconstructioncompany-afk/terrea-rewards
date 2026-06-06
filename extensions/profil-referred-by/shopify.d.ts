e40ec048c6481c8bef1a1a36c434d432b5866f94
David <aygedzorconstructioncompany@gmail.com>
2026-06-06 19:20:40 +0400

    fix: single target two s-text

--- extensions/profil-referred-by/shopify.d.ts
+++ extensions/profil-referred-by/shopify.d.ts
@@ -5,3 +5,9 @@ declare module './src/OrderStatusBlock.jsx' {
   const shopify: import('@shopify/ui-extensions/customer-account.profile.block.render').Api;
   const globalThis: { shopify: typeof shopify };
 }
+
+//@ts-ignore
+declare module './src/ReferredBy.jsx' {
+  const shopify: import('@shopify/ui-extensions/customer-account.profile.block.render').Api;
+  const globalThis: { shopify: typeof shopify };
+}
